ad4367547cf4d1adfdcc086a3bdd0246fe66801a
anthony-pinhooo <contato.anthony27@gmail.com>
2022-05-16 21:59:44 -0400

    inserindo o arquivo inicial[C

---
+++ simulador_de_dados.py
@@ -0,0 +1,10 @@
+import random
+
+class SimuladorDado:
+    def __init__(self):
+        self.valorMinimo = 1
+        self.valorMaximo = 6
+        self.mensagem = "Você gostaria de girar o dado novamente ?"
+    
+    def Iniciar(self):
+        
