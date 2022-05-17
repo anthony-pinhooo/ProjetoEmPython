4852edd561f158b4f11ce3baf4e73c7e482f4d6e
anthony-pinhooo <contato.anthony27@gmail.com>
2022-05-16 22:29:01 -0400

    código atualizado e finalizado

--- simulador_de_dados.py
+++ simulador_de_dados.py
@@ -4,7 +4,26 @@ class SimuladorDado:
     def __init__(self):
         self.valorMinimo = 1
         self.valorMaximo = 6
-        self.mensagem = "Você gostaria de girar o dado novamente ?"
+        self.mensagem = "Você gostaria de girar o dado novamente ?\n"
     
     def Iniciar(self):
+        resposta = input(self.mensagem)
+        try:
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado()
+                
+            elif resposta == 'não' or resposta == 'nao' or resposta == 'n':
+                print("Ok ! Obrigado por participar !")     
+                
+            else:
+                print("Favor, digitar sim ou não")
+                
+        except:
+            print("Erro ! Ocorreu um erro ao receber sua resposta")
+            
+    def GerarValorDoDado(self):
+        print(random.randint(self.valorMinimo, self.valorMaximo))
         
+        
+simulador = SimuladorDado()
+simulador.Iniciar()
